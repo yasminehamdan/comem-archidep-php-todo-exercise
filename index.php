8d97a065c86a3203880a6886e498398915d499dc
Simon Oulevay <simon.oulevay@gmail.com>
2019-01-11 16:07:41 +0100

    New title

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ $items = $db->query($selectQuery);
 
 <html>
   <head>
-    <title>TodoList</title>
+    <title>Amazing TodoList</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
