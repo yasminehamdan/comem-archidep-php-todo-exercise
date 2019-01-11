20b8515b3fe5f9d66a8b798f2f20360ed4516c61
Simon Oulevay <simon.oulevay@gmail.com>
2019-01-11 16:08:32 +0100

    New title

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ $items = $db->query($selectQuery);
       <div class="navbar navbar-dark bg-dark shadow-sm">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <strong>TodoList</strong>
+            <strong>Amazing TodoList</strong>
           </a>
         </div>
       </div>
